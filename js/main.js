d8363bd56fed7e2f8aedbdbf2278c24eca8579fa
seonmin <seonmin1414@gmail.com>
2024-09-12 14:27:23 +0900

    900px 반응형 추가 및 전체적인 레이아웃 수정

--- js/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function(){
-    const closeBtn = this.document.querySelector(".closeBtn")
-    const firstWindow = this.document.querySelector(".first-window")
-    closeBtn.addEventListener("click",function(){
-        firstWindow.style.display = "none"
-    })
-})

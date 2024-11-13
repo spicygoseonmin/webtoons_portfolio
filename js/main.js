bc46eb0ab836df316a6b63a4a07785ef74174249
DESKTOP-RLIRLHO\user <seonmin1414@gmail.com>
2024-11-13 10:57:54 +0900

    모달창 생성

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+    const body = this.document.querySelector("body")
+    const modal = this.document.querySelector(".modal_wrap")
+    const close_modal = this.document.querySelector(".close_modal")
+    close_modal.addEventListener("click", function(){
+        modal.style.display = "none"
+    })
+})

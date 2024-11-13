ab2f12bb001f42f7d31f469df0abae46b08a8937
DESKTOP-RLIRLHO\user <seonmin1414@gmail.com>
2024-11-13 17:00:52 +0900

    오늘 하루 보지 않기 모달창 생성

--- js/modal.js
+++ js/modal.js
@@ -1,49 +1,36 @@
+$(function() {
+	// $(".modal_wrap").draggable({containment: 'parent', scroll: false}); // 레이어 팝업 창 드래그 가능
 
-var toggleMainPopup = function() {
-  
-  /* 스토리지 제어 함수 정의 */
-  var handleStorage = {
-    // 스토리지에 데이터 쓰기(이름, 만료일)
-    setStorage: function (name, exp) {
-      // 만료 시간 구하기(exp를 ms단위로 변경)
-      var date = new Date();
-      date = date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+	// 쿠키 저장여부 체크
+	if (getCookie("popToday") !== "close") {      
+		document.querySelector(".modal_wrap").style.display = "block";
+	} else {
+		document.querySelector(".modal_wrap").style.display = "none"; 
+	}
+});
 
-      // 로컬 스토리지에 저장하기
-      // (값을 따로 저장하지 않고 만료 시간을 저장)
-      localStorage.setItem(name, date)
-    },
-    // 스토리지 읽어오기
-    getStorage: function (name) {
-      var now = new Date();
-      now = now.setTime(now.getTime());
-      // 현재 시각과 스토리지에 저장된 시각을 각각 비교하여
-      // 시간이 남아 있으면 true, 아니면 false 리턴
-      return parseInt(localStorage.getItem(name)) > no
-    }
-  };
-  
-  
-  // 쿠키 읽고 화면 보이게
-  if (handleStorage.getStorage("today")) {
-    $(".modal_wrap").removeClass("on");
-  } else {
-    $(".modal_wrap").addClass("on");
-  }
-
-  // 오늘하루 보지 않기 버튼
-  $(".modal_wrap").on("click", ".close_modal", function () {
-    // 로컬 스토리지에 today라는 이름으로 1일(24시간 뒤) 동안 보이지 않게
-    handleStorage.setStorage("today", 1);
-    $(this).parents(".modal_wrap.on").removeClass("on");
-  });
-
-  // 일반 닫기 버튼
-//   $(".modal_wrap").on("click", ".btn_close", function () {
-//     $(this).parents(".modal_wrap.on").removeClass("on");
-//   });
+// 오늘하루만보기 닫기버튼 스크립트
+function closeToday() { 
+	setCookie("popToday", "close", 1); 
+	document.querySelector(".modal_wrap").style.display = "none";
 }
 
-$(function() {
-  toggleMainPopup();
-});
+// 그냥 닫기버튼 스크립트
+function closePop() { 
+	document.querySelector(".modal_wrap").style.display = "none";
+}
+
+// 쿠키 설정 함수
+function setCookie(name, value, days) {
+	let date = new Date();
+	date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+	document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/`;
+}
+
+// 쿠키 가져오기 함수
+function getCookie(name) {
+	const value = `; ${document.cookie}`;
+	const parts = value.split(`; ${name}=`);
+	if (parts.length === 2) return parts.pop().split(';').shift();
+	return null;
+}
